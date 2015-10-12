42c98b5d463a49adb95a1961bf5c5f28ca500e1e
Christian Silver <christian@decode.org.nz>
2015-10-12 20:07:00 +0100

    Save users to database

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+'use strict';
+
 var express = require('express');
-var Browser = require('zombie');
 var nunjucks = require('nunjucks');
-var UserMocker = require('lib/user-mocker');
-var cheerio = require('cheerio');
+var mongoose = require('mongoose');
+require('dotenv').load();
 var app = express();
 
-
+mongoose.connect(process.env.MONGO_URL);
 
 var env = nunjucks.configure('views', {
   autoescape: true,
@@ -14,27 +15,15 @@ var env = nunjucks.configure('views', {
 
 env.addFilter('calEscape', function (text) {
   return text
-    .replace(/\,\;/g, function (match) { return '\\' + match })
+    .replace(/\,\;/g, function (match) { return '\\' + match; })
     .replace(/(\r\n|\n|\r)/gm, '\\n');
 });
 
 app.get('/', function (req, res) {
-  var user = new UserMocker();
-
-  user
-    // .loginFromSomething
-    .done(function () {
-      console.log(JSON.stringify({ cok: user.getCookies() }));
-      user.getMealsInfo().then(function (infos) {
-        console.log(infos);
-        res.render('calendar.ical', {
-          email: 'cas217@cam.ac.uk',
-          user: 'cas217',
-          events: infos
-        });
-      });
-    });
+  res.render('index.html');
 });
+
+app.use('/api', require('./routes/api'));
 
 var listener = app.listen(3000, function () {
   console.log('Listening on port ' + listener.address().port);
---
+++ routes/api.js
@@ -0,0 +1,115 @@
+'use strict';
+
+var express = require('express');
+var User = require('lib/user');
+var UserMocker = require('lib/user-mocker');
+var bodyParser = require('body-parser');
+var _ = require('lodash');
+
+var api = new express.Router();
+
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({ extended: true }));
+
+api.get('/calendar/:slug', function (req, res, next) {
+  var slug = req.params.slug;
+  var userMocker = new UserMocker();
+  var user;
+  var infos;
+
+  User.findOne({ slug: slug })
+    .then(function (u) {
+      if (!u) {
+        var err = new Error('We don\'t have that calendar');
+        err.status = 404;
+        throw err;
+      }
+
+      user = u;
+      return userMocker.loginFromCookies(user.cookies);
+    })
+    .then(function () {
+      return userMocker.getMealsInfo();
+    })
+    .then(function (i) {
+      infos = i;
+      user.cookies = userMocker.getCookies();
+      return user.save();
+    })
+    .then(function () {
+      res.render('calendar.ical', {
+        email: user.getEmail(),
+        user: user.camId,
+        events: infos
+      });
+    })
+    .catch(function (e) {
+      // TODO: email user and notify of error
+      next(e);
+    });
+});
+
+api.post('/generate-calendar', function (req, res, next) {
+  if (!_.has(req.body, 'username')) {
+    let err = new Error('Must provide username.');
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  if (!_.has(req.body, 'password')) {
+    let err = new Error('Must provide password.');
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  var userMocker = new UserMocker();
+  var user;
+
+  userMocker.loginFromCredentials(req.body.username, req.body.password)
+    .catch(function () {
+      var err = new Error('Incorrect login details.');
+      err.status = 401;
+      next(err);
+    })
+    .then(function () {
+      return userMocker.getMealsInfo()
+        .then(function (infos) {
+          return User.findOne({ _camId: req.body.username });
+        })
+        .then(function (u) {
+          if (!u) {
+            u = new User({
+              camId: req.body.username
+            });
+          }
+
+          user = u;
+          user.cookies = userMocker.getCookies();
+
+          return user.save();
+        })
+        .then(function () {
+          res.json({ url: 'webcal://' + req.get('host') + req.baseUrl + '/calendar/' + user.slug });
+        });
+    })
+    .catch(function (err) {
+      next(new Error('Something went wrong'));
+    });
+});
+
+api.use(function (req, res, next) {
+  var err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+api.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    error: err.message || 'An error occurred'
+  });
+});
+
+module.exports = api;
