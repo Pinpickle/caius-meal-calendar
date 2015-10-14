787650f4dddfb9ff66b38f43568bc3227da1556a
Christian Silver <christian@decode.org.nz>
2015-10-14 14:49:17 +0100

    Add SSL support

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ require('dotenv').load();
 
 var express = require('express');
 var mongoose = require('mongoose');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
 var app = express();
 
 require('./config/views')(app);
 require('./config/mailer')(app);
+
+var sslKey = null;
+var sslCert = null;
+var sslCa = null;
+var ssl = true;
+
+try {
+  sslKey = fs.readFileSync('./config/server.key');
+  sslCert = fs.readFileSync('./config/server.crt');
+  sslCa = fs.readFileSync('./config/ca.crt');
+} catch (e) {
+  ssl = false;
+}
 
 mongoose.connect(process.env.MONGO_URL);
 
@@ -21,6 +37,20 @@ app.get('/', function (req, res) {
 
 app.use('/api', require('./routes/api'));
 
-var listener = app.listen(3000, function () {
+var server;
+
+if (ssl) {
+  server = https.createServer({
+    key: sslKey,
+    cert: sslCert,
+    ca: sslCa,
+    requestCert: true,
+    rejectUnauthorized: false
+  }, app);
+} else {
+  server = http.createServer(app);
+}
+
+var listener = server.listen(3000, function () {
   console.log('Listening on port ' + listener.address().port);
 });
